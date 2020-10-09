f83cde9a5a1f4c55acbd9d935161e289b9e3d08e
Noah Willen <nwillen@live.unc.edu>
2020-10-09 13:03:41 -0400

    heroku with diagnal movement

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res) {
 
 app.use('/client', express.static(__dirname + '/client'))
 
-serv.listen(2000, hostname);
+serv.listen(process.env.PORT || 2000);
 console.log("Server Started")
 
 
@@ -41,11 +41,14 @@ var Player = function(id){
     self.move = function(){
         if (self.pressRight) {
             self.x+= self.spd
-        } else if (self.pressLeft) {
+        }
+        if (self.pressLeft) {
             self.x-= self.spd
-        } else if (self.pressDown) {
+        }
+        if (self.pressDown) {
             self.y -= self.spd
-        } else if (self.pressUp) {
+        }
+        if (self.pressUp) {
             self.y += self.spd
         }
     }
@@ -90,11 +93,14 @@ io.sockets.on('connection', function(socket) {
     socket.on('keyPress', function(data){
         if (data.InputId == "right") {
             player.pressRight = data.state
-        } else if (data.InputId == "left") {
+        }
+        if (data.InputId == "left") {
             player.pressLeft = data.state
-        } else if (data.InputId == "up") {
+        }
+        if (data.InputId == "up") {
             player.pressUp = data.state
-        } else if (data.InputId == "down") {
+        }
+        if (data.InputId == "down") {
             player.pressDown = data.state
         }
     })
